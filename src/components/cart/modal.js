a109ec559f83d52981abe56399012aac5f08ce48
Pocho <leooinsa79@gmail.com>
2022-12-27 02:49:18 -0300

    ultimo retoque

--- src/components/cart/modal.js
+++ src/components/cart/modal.js
@@ -1,16 +1,4 @@
 
-// const cerrarCarro= document.getElementById("btn__cerrarcarro")[0];
-// const abrirCarro = document.getElementById("carrito__cesto");
-// const modalCarro = document.querySelector(".modal__carro")[0];
-// const modalContainer = document.querySelector(".modal__contenedor")[0];
-
-// abrirCarro.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     modalContainer.style.opacity= "0.99";
-//     modalContainer.style.visibility = "visible";
-//     modal.classList.toggle("modal__close");
-// });
-
 
 const modalContainer = document.querySelector(".modal__contenedor");
 const abrirCarro = document.getElementById("carrito__cesto");
